13087de5c040b5d381e8cbc99cc530a7d581bac6
Florian König <koenidv@gmail.com>
2023-10-15 17:06:52 +0200

    feat: identicon endpoint

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var identiconRouter = require('./routes/identicon');
 
 var app = express();
 
@@ -21,6 +22,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/identicon', identiconRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ routes/identicon.js
@@ -0,0 +1,20 @@
+var express = require('express');
+const jdenticon = require("jdenticon");
+var router = express.Router();
+
+router.get('/', function(req, res) {
+    let size = req.query.size || 200;
+    const value = req.query.value || "koenidv";
+    
+    if (req.format === "svg") {
+        size = Math.max(size, 10240)
+        res.set("Content-Type", "image/svg+xml");
+        res.send(jdenticon.toSvg(value, size));
+    } else {
+        size = Math.max(size, 1024)
+        res.set("Content-Type", "image/png");
+        res.send(jdenticon.toPng(value, size));
+    }
+});
+
+module.exports = router;
